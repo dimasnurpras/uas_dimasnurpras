d9e79c7c59f62a4b5879a827d33d45e53dc22533
dimasnurpras <99072663+dimasnurpras@users.noreply.github.com>
2022-02-05 17:09:03 +0700

    Add files via upload

---
+++ UASdimas/unegodb.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 05 Feb 2022 pada 10.55
+-- Versi server: 10.4.14-MariaDB
+-- Versi PHP: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `unegodb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `harga` varchar(40) NOT NULL,
+  `stok` varchar(40) NOT NULL,
+  `foto` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`id`, `nama`, `harga`, `stok`, `foto`) VALUES
+(1, 'RAXZEL HOODIE', '40000', '100', 'raxzel-1.jpg'),
+(2, 'MONOKROM HOODIE', '120000', '99', 'monokrom-1.jpg'),
+(3, '3SECOND HOODIE', '50000', '100', '3second-1.jpg'),
+(6, 'RAXZEL T-SHIRT', '50000', '100', 'raxzel-2.jpg'),
+(7, 'MONOCHROME T-SHIRT', '50000', '100', 'monokrom-2.jpg'),
+(8, '3SECOND T-SHIRT', '50000', '100', '3second-2.jpg'),
+(9, 'TAS RAXZEL ', '80000', '100', 'raxzel-3.jpg'),
+(10, 'TAS 3SECOND ', '90000', '100', '3second-3.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
